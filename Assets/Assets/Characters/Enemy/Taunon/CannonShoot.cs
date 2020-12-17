71401b98df8a172800be9242277ae4d63a7a9f48
pielloy <pielloy20@gmail.com>
2020-12-17 10:37:51 +0100

    enemy

---
+++ Assets/Assets/Characters/Enemy/Taunon/CannonShoot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonShoot : MonoBehaviour
+{
+    public GameObject myPrefab;
+
+    public void Shoot(string s)
+    {
+        Vector3 ball_pos = transform.Find("CanonArmature/Root/Root.002/Root.003/Root.004/Root.005/Root.005_end/ShootPosition").transform.position;
+        Quaternion ball_rotation = transform.rotation;
+
+        Instantiate(myPrefab, ball_pos, ball_rotation);
+    }
+
+    void Update()
+    {
+
+    }
+}
