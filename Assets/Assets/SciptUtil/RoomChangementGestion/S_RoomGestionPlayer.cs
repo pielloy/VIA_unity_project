19a49e48acbe42396cd72bb82ef2c02e3a7c9d3e
pielloy <pielloy20@gmail.com>
2021-01-18 20:50:10 +0100

    arg

--- Assets/Assets/SciptUtil/RoomChangementGestion/S_RoomGestionPlayer.cs
+++ Assets/Assets/SciptUtil/RoomChangementGestion/S_RoomGestionPlayer.cs
@@ -8,16 +8,14 @@ using UnityEngine.Rendering.Universal;
 public class S_RoomGestionPlayer : MonoBehaviour
 {
     public ArrayList m_Rooms = new ArrayList();
-    private ColorAdjustments m_CALayer;
+    private Volume m_postProcessing;
+    private float m_value;
+    public float m_speed = 0.01f;
 
     void Awake()
     {
-        Volume m_postProcessing;
-
-        foreach (GameObject element in GameObject.FindGameObjectsWithTag("PostProcessing"))
-        {
-            
-        }
+        m_postProcessing = GameObject.FindGameObjectsWithTag("PostProcessing")[0].GetComponent<Volume>();
+        enabled = false;
     }
 
     public void AddRoom(GameObject room)
@@ -29,12 +27,20 @@ public class S_RoomGestionPlayer : MonoBehaviour
     {
         m_Rooms.Remove(room);
         room.transform.Find("Content").transform.gameObject.SetActive(false);
-        if (m_Rooms.Count == 1)
+        if (m_Rooms.Count == 1 && !((GameObject)m_Rooms[0]).transform.Find("Content").transform.gameObject.activeSelf) {
             ((GameObject)m_Rooms[0]).transform.Find("Content").transform.gameObject.SetActive(true);
+            enabled = true;
+            m_value = 1;
+        }
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        
+        m_value -= m_speed;
+        if (m_value <= 0) {
+            enabled = false;
+            m_value = 0;
+        }
+        m_postProcessing.weight = m_value;
     }
 }
